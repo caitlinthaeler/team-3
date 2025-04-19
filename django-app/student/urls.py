7b9e4f9989e86cae89f7659bc593aa48aeba3705
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-04-19 16:45:34 +0100

    student routes working

--- django-app/student/urls.py
+++ django-app/student/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('chat/', views.chat, name='chat'),
+    path('game/', views.game, name='game'),
+    path('task/', views.task, name='task'),
+    path('settings/', views.settings, name='settings'),
+    path('tutorial/', views.tutorial, name='tutorial'),
 ]
--- django-app/student/views.py
+++ django-app/student/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'homePage.html')
+
+def chat(request):
+    return render(request, 'chatPage.html')
+
+def game(request):
+    return render(request, 'gamePage.html')
+
+def task(request):
+    return render(request, 'taskPage.html')
+
+def settings(request):
+    return render(request, 'settingsPage.html')
+
+def tutorial(request):
+    return render(request, 'tutorialPage.html')
